8081984b2f17b09366a196a37202aa465dd9ace4
Dipti Jain <deeptihnd000@gmail.com>
2024-06-28 20:32:42 +0530

    created popluar component

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
-function Item() {
-  return <div></div>;
+import "../index.css";
+function Item(props) {
+  return (
+    <div className="item">
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">${props.new_price}</div>
+        <div className="item-price-old">${props.old_price}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Item;
