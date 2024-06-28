3d85714b1603506fbe74d47da485ac245efcf938
Dipti Jain <deeptihnd000@gmail.com>
2024-06-28 17:25:31 +0530

    set link on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Shop, ShopCategory } from "./Pages";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import Login from "./Pages/Login";
 
 function App() {
   return (
@@ -10,9 +14,14 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory />} />
-          <Route path="/womens" element={<ShopCategory />} />
-          <Route path="/kids" element={<ShopCategory />} />
+          <Route path="/mens" element={<ShopCategory category="men" />} />
+          <Route path="/womens" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
+import { Link } from "react-router-dom";
 import logo from "./Assets/logo.png";
 import cart_icon from "./Assets/cart_icon.png";
 function Navbar() {
   const [menu, setMenu] = useState("shop");
-  const navbarItem = (item) => {
-    console.log(item);
-    return (
-      <li onClick={() => setMenu(item)}>
-        {item.charAt(0).toUpperCase() + item.slice(1)}
-        <hr style={{ display: menu === item ? "block" : "none" }} />
-      </li>
-    );
-  };
+
   const list = ["shop", "men", "women", "kids"];
   return (
     <div className="navbar">
@@ -20,10 +13,39 @@ function Navbar() {
         <img src={logo} alt="Site Logo" />
         <p>SHOPPER</p>
       </div>
-      <ul className="nav-menu">{list.map((item) => navbarItem(item))}</ul>
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("shop")}>
+          <Link style={{ textDecoration: "none" }} to="/">
+            Shop
+          </Link>
+          <hr style={{ display: menu === "shop" ? "block" : "none" }} />
+        </li>
+        <li onClick={() => setMenu("men")}>
+          <Link style={{ textDecoration: "none" }} to="/mens">
+            Men
+          </Link>
+          <hr style={{ display: menu === "men" ? "block" : "none" }} />
+        </li>
+        <li onClick={() => setMenu("women")}>
+          <Link style={{ textDecoration: "none" }} to="/womens">
+            Women
+          </Link>
+          <hr style={{ display: menu === "women" ? "block" : "none" }} />
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          <Link style={{ textDecoration: "none" }} to="/kids">
+            Kids
+          </Link>
+          <hr style={{ display: menu === "kids" ? "block" : "none" }} />
+        </li>
+      </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="Add to Cart" />
+        <Link style={{ textDecoration: "none" }} to="/login">
+          <button>Login</button>
+        </Link>
+        <Link style={{ textDecoration: "none" }} to="/cart">
+          <img src={cart_icon} alt="Add to Cart" />
+        </Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
