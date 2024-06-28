412431fb269b1161b9d911e04502fd30b7cdec30
Dipti Jain <deeptihnd000@gmail.com>
2024-06-28 17:03:33 +0530

    set routes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Shop, ShopCategory } from "./Pages";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory />} />
+          <Route path="/womens" element={<ShopCategory />} />
+          <Route path="/kids" element={<ShopCategory />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
